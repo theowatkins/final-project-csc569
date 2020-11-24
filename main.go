f7c08a48093524baf8ad3e44303f10e7682e0b14
theowatkins <theowatkins1@gmail.com>
2020-11-23 22:43:06 -0500

    starter code

--- main.go
+++ main.go
@@ -1,12 +1,58 @@
 package main
 
-import "./vector"
+import "./types"
 import "fmt"
 
-const VECSIZE = vector.VECSIZE
+const CLUSTER_SIZE = types.CLUSTER_SIZE
+
+// early todos:
+//  - How are we going to do client communication? (who's sending messages)
+//  - What do we do with a message when it's processed?
 
 func main() {
-	vec1 := [VECSIZE]int{1,1,0}
-	vec2 := [VECSIZE]int{2,2,1}
-	fmt.Println(vector.LTE(vec1, vec2))
+    init_cluster()
+    fmt.Println("done")
+}
+
+func init_cluster() {
+	var com_chans [CLUSTER_SIZE]chan types.Message
+
+	// Set up communication channels
+	for i:=0; i<CLUSTER_SIZE; i++ {
+		com_chans[i] = make(chan types.Message)
+		go start_server(i, &com_chans)
+	}
+}
+
+func start_server(id int, com_chans *[CLUSTER_SIZE]chan types.Message) {
+    var time [CLUSTER_SIZE]int
+    log := make([]string, 0)
+
+    // Make state
+    server_state := types.ServerState{
+        id, // Server ID
+        log, // Empty message log
+        time} // Vector time initialized to 0
+
+	// TODO: send messages (broadcast) and update vector time
+	go receive_messages(&server_state, com_chans)
+}
+
+func receive_messages(server_state *types.ServerState, com_chans *[CLUSTER_SIZE]chan types.Message) {
+	select {
+	case message := <-com_chans[server_state.Id]:
+		if message.Type == types.RegularM {
+            // TODO: out of order messages
+
+            // Update local time
+            server_state.LocalTime[message.Sender] = message.Timestamp[message.Sender]
+
+            // Print message for now
+            // TODO: what are we gonna do with messages?
+            fmt.Println("Server ", server_state.Id, "received message: ")
+            fmt.Println(message.Message, "\n")
+		} else if message.Type == types.ResendM {
+            // TODO: look into log and find message with correct timestamp (if it exists)
+		}   
+	}
 }
---
+++ types/types.go
@@ -0,0 +1,32 @@
+package types
+
+type MessageType string
+const RegularM MessageType = "REGULAR"
+const ResendM MessageType = "RESEND"
+
+const CLUSTER_SIZE = 3
+
+type Message struct {
+
+	// Regular or resend
+	Type MessageType
+
+	// REGULAR: Text being sent
+	// RESEND: Empty if the message is requesting a resend
+	Message string
+
+	// REGULAR: Whoever is sending the message
+	// RESEND: Whoever is sending the resend request
+	Sender int
+
+	// REGULAR: Timestamp of the text associated with the message
+	// RESEND: Timestamp of the message being requested
+	Timestamp [CLUSTER_SIZE]int
+}
+
+
+type ServerState struct {
+	Id int
+	MessageLog []string
+	LocalTime [CLUSTER_SIZE]int
+}
